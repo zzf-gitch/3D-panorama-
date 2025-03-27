d569c3558dba3979f54678af313cb5e68d0b189a
zhangzhuofei1 <z15903509849@163.com>
2025-03-27 13:56:45 +0800

    3D-panorama

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+  /* // background-image: url(assets/wappler.jpg); */
+  /* // background-repeat: no-repeat; */
+  /* // background-size: 100% 100%; */
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div id="viewer"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref , reactive , onMounted} from 'vue'
+import { Viewer } from 'photo-sphere-viewer' // 引入插件
+import 'photo-sphere-viewer/dist/photo-sphere-viewer.css' //引入CSS样式
+// 全景视图
+const  panoramaViewer = ref(null)
+const img = ref(require('../assets/BJ.png'))
+// 全景图初始化
+const initViewer = async function () {
+  panoramaViewer.value = new Viewer({
+    container: document.querySelector('#viewer'), // 容器
+    panorama: img.value, // 全景图片地址
+    navbar: [
+      'autorotate',
+      'zoom',
+      'caption',
+      'fullscreen'
+    ],
+    plugins: [], // 标记点
+    size: {
+      width: '100%',
+      height: '100%'
+    }
+  })
+}
+
+onMounted(() => {
+  initViewer()
+})
+
+</script>
+
+<style scoped>
+.home{
+  width: 100%;
+  height: 100%;
+}
+</style>
